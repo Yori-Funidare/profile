3d249890b3ae1906794e9d94af8cbe9b6d9c55b1
Yori-Funidare <triodiory@gmail.com>
2025-07-30 17:53:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const text = "Ini bukan informasi pribadi saya!, namun saya melampirkan beberapa bahasa yang saya gunakan untuk membangun page ini serta beberapa akun sosial media saya.";
+let index = 0;
+
+function ketik(){
+  if (index < text.length){
+    document.getElementById("type").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(ketik, 20);
+  }
+}
+ketik()
